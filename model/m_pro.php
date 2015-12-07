42432670c1bf9a06f186ce7fb814cbb13771022e
Seul <Seul@Seul-PC>
2015-12-07 23:58:50 +0800

    1

--- model/m_pro.php
+++ model/m_pro.php
@@ -3,9 +3,14 @@ class m_pro extends spModel{
 	var $pk = "id";
 	var $table = "fstk_pro";
         function getmypage($conditions,$order, $page, $pageSize){
-           return $this->spPager($page,$pageSize)->findAll($conditions,$order);
+           $result['data'] = $this->spPager($page,$pageSize)->findAll($conditions,$order);
+           $result['pager'] = $this->spPager()->getPager();
+           return  $result;
         }
-        function getalldata($conditions,$order){
+        function getC1data($conditions,$order){
+            return $this->findAll($conditions,$order);
+        }
+        function getC2data($conditions,$order){
             return $this->findAll($conditions,$order);
         }
 }
